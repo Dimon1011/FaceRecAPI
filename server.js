4d88e50155d084ba5fda6b90c4eedbdfd932e904
Dima <dima@MacBook-Air-3.hitronhub.home>
2020-06-15 18:36:37 -0600

    Add files for API

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: '',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send(database.users) })
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.post('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+// LISTEN PORT 3000
+app.listen(3000, () => {
+    console.log('.....App is runing on port: 3000......')
+})
+
